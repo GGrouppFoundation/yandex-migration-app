43e9f3725fecaa020a59e71115d984c03fbe34fd
pmosk <pm@ggroupp.ru>
2025-05-27 23:35:21 +0300

    Use UserCache in TrackerUserGet method

--- src/service/Tracker/Api/Api/TrackerApi.cs
+++ src/service/Tracker/Api/Api/TrackerApi.cs
@@ -1,8 +1,15 @@
+using System.Collections.Concurrent;
 using GarageGroup.Infra;
 
 namespace GGroupp.Yandex.Migration;
 
 internal sealed partial class TrackerApi(IHttpApi httpApi) : ITrackerApi
 {
-    private const string OrganizationsUrl = "https://organization-manager.api.cloud.yandex.net/organization-manager/v1/organizations";
+    private const string OrganizationsUrl
+        =
+        "https://organization-manager.api.cloud.yandex.net/organization-manager/v1/organizations";
+
+    private readonly ConcurrentDictionary<TrackerUserGetIn, TrackerUserGetOut> UserCache
+        =
+        new();
 }
