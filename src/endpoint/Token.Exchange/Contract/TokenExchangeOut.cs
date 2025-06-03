53a4a9e5a09bc4d257a0ecc8dea4a05e49a6add5
sokolawesome <sokolawesome@gmail.com>
2025-06-03 14:09:09 +0300

    fix: required string

--- src/endpoint/Token.Exchange/Contract/TokenExchangeOut.cs
+++ src/endpoint/Token.Exchange/Contract/TokenExchangeOut.cs
@@ -6,7 +6,7 @@ namespace GGroupp.Yandex.Migration;
 public sealed record class TokenExchangeOut
 {
     [JsonBodyOut]
-    public string IamToken { get; init; } = string.Empty;
+    public required string IamToken { get; init; }
 
     [JsonBodyOut]
     public DateTime ExpiresAt { get; init; }
